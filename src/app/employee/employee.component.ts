04011b49f1e36af34111cd72102b4142879b1fd3
amoore16 <amoore16@gmail.com>
2018-07-31 21:53:01 -0600

    employee service, proper imports

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { EmployeeService } from '../shared/employee.service';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css'],
+  providers: [EmployeeService]
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this.resetForm();
+  }
+
+  resetForm(form?: NgForm) {
+    if(form)
+      form.reset();
+    this.employeeService.selectedEmployee = {
+      _id: "",
+      name: "",
+      position: "",
+      office: "",
+      salary: null
+
+    }
+  }
+
+}
